9d89f1173a9b0ec12a3e5473c989793b47172afc
remcodekoeijer <remcodekoeijer@hotmail.com>
2018-10-08 09:40:05 +0200

    Real first commit?

---
+++ src/main/java/com/XYZairlines/XYZairlines/XyZairlinesApplication.java
@@ -0,0 +1,12 @@
+package com.XYZairlines.XYZairlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyZairlinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyZairlinesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/XYZairlines/XYZairlines/XyZairlinesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.XYZairlines.XYZairlines;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XyZairlinesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
